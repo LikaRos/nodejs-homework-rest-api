5f2079da940f99a8f9aa23f0fa228b898125d687
Lika <lika.rossinskaya@gmal.com>
2022-10-13 09:11:35 +0300

    init project

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
-const express = require("express");
-const logger = require("morgan");
-const cors = require("cors");
+const mongoose = require("mongoose");
+const { DB_HOST } = require("./config");
 
-const contactsRouter = require("./routes/api/contacts");
-const app = express();
-const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connect success"))
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
 
-app.use(logger(formatsLogger));
-app.use(cors());
-app.use(express.json());
+// const express = require("express");
+// const logger = require("morgan");
+// const cors = require("cors");
 
-app.use("/api/contacts", contactsRouter);
-app.set("json space", 8); //метод  експресу  форматує  формат json
+// const contactsRouter = require("./routes/api/contacts");
+// const app = express();
+// const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-app.use((req, res) => {
-  res.status(404).json({ message: "Not found" });
-});
+// app.use(logger(formatsLogger));
+// app.use(cors());
+// app.use(express.json());
 
-app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error" } = err;
-  res.status(status).json({ message });
-});
+// app.use("/api/contacts", contactsRouter);
+// app.set("json space", 8); //метод  експресу  форматує  формат json
+
+// app.use((req, res) => {
+//   res.status(404).json({ message: "Not found" });
+// });
+
+// app.use((err, req, res, next) => {
+//   const { status = 500, message = "Server error" } = err;
+//   res.status(status).json({ message });
+// });
 
 module.exports = app;
