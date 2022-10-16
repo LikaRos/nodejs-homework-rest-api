4a69a72333f67858dbaeed41d322847ea21b35a6
Lika <lika.rossinskaya@gmal.com>
2022-10-16 09:44:37 +0300

    pagination  added

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -1,7 +1,7 @@
 const { Contact } = require("../../models/contact");
 
 const listContacts = async (req, res) => {
-  const { page = 1, limit = 2 } = req.query;
+  const { page = 1, limit = 20 } = req.query;
   const skip = (page - 1) * limit;
   const result = await Contact.find(req.user, "-createdAt -updatedAt", {
     skip,
